6062d90d90fafb4c4b0f38902ac786c61fe22c60
muurastr <muura@mail.ru>
2016-05-20 14:20:10 +0300

    Add test for car

---
+++ car_spec.rb
@@ -0,0 +1,12 @@
+require './car' #требование включения файла car
+#    . - это текущий каталог
+describe Car do
+  it "must return range" do
+#arrange
+    car = Car.new
+#act
+    car.add_fuel 10
+#assert
+    expect(car.range).to eq 200
+  end
+end
